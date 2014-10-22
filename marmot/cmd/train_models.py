04a277b6f84a8c9cf6402a04fc4ec32d9731aed2
muelletm@gmail.com <muelletm@gmail.com@a95995bb-a1a2-3262-07d2-502f3465652d>
2014-10-22 19:10:37 +0000

    Made train script select latest jar.

--- marmot/cmd/train_models.py
+++ marmot/cmd/train_models.py
@@ -1,10 +1,16 @@
 #! /usr/bin/env python
 
+import glob
 import json
-import subprocess
 import os
+import subprocess
+import sys
+
+MARMOT_DIR = ".."
 
 def train(data, jarfile):
+
+
     lang = data['lang']
     modelfile = '%s.marmot' % lang
     logfile = '%s.log' % lang
@@ -13,16 +19,28 @@ def train(data, jarfile):
     cmd_string = """ java -cp %(jar)s marmot.morph.cmd.Trainer 
                       -train-file %(train)s 
                       -model-file %(model)s
-                      -num-iterations 1
                       > %(log)s
                       2>&1
           """ % { 'jar' : jarfile, 'train' : trainfile, 'model' : modelfile, 'log' : logfile }
 
+    print lang, trainfile
     subprocess.check_call(cmd_string.replace('\n',' '), shell=True)
     assert os.path.exists(modelfile)
 
+def get_jarfile(dirname):
+    jars = glob.glob(os.path.join(dirname, 'marmot*.jar'))
+
+    if not jars:
+        print >> sys.stderr, 'Error: Did not find marmot*.jar in "%s".' % dirname
+        sys.exit(1)
+
+    jars.sort()
+    return jars[-1]
+
 if __name__ == '__main__':
+
+    jarfile = get_jarfile(MARMOT_DIR)
 
     with open('data.json') as f:
         for lang_data in json.load(f):
-            train(lang_data, '../marmot-2014-10-22.jar')
+            train(lang_data, jarfile)
