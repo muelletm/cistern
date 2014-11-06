6f80ea63efeee7ba74808b26b0de4446cb4e814d
muelletm@gmail.com <muelletm@gmail.com@a95995bb-a1a2-3262-07d2-502f3465652d>
2014-11-06 10:42:54 +0000

    Renamed OpenNlpScriptTest into TransformatorTest. Created a new Aligner and a test class. Some minor changes to LevenshteinLattice in order to make it more flexible. Also fixed a bug in Levenshtein lattice (regarding costs different from 1).

--- trunk/marmot/src/marmot/test/LevenshteinLatticeTest.java
+++ trunk/marmot/src/marmot/test/LevenshteinLatticeTest.java
@@ -12,6 +12,71 @@ import org.junit.Test;
 public class LevenshteinLatticeTest {
 
 	@Test
+	public void costTest() {
+		String input, output, expected, actual;
+		LevenshteinLattice lattice;
+		
+		input = "A C B";
+		output = "A D B";
+		lattice = new LevenshteinLattice(input, output, 2, 2, 3);
+		actual = lattice.searchOperationSequence();
+		expected = "CCRCC";
+		assertEquals(expected, actual);
+		assertEquals(3, lattice.getDistance());
+		
+		input = "A C B";
+		output = "A DD B";
+		lattice = new LevenshteinLattice(input, output, 2, 2, 3);
+		actual = lattice.searchOperationSequence();
+		expected = "CCIRCC";
+		assertEquals(expected, actual);
+		assertEquals(5, lattice.getDistance());
+		
+		input = "A C B";
+		output = "A DDDD B";
+		lattice = new LevenshteinLattice(input, output, 2, 2, 3);
+		actual = lattice.searchOperationSequence();
+		expected = "CCIIIRCC";
+		assertEquals(expected, actual);
+		assertEquals(9, lattice.getDistance());
+		
+		input = "A C B";
+		output = "A DDDDD B";
+		lattice = new LevenshteinLattice(input, output, 2, 2, 3);
+		actual = lattice.searchOperationSequence();
+		expected = "CCIIIIRCC";
+		assertEquals(expected, actual);
+		assertEquals(11, lattice.getDistance());
+		
+		input = "A C B";
+		output = "A DDDDDD B";
+		lattice = new LevenshteinLattice(input, output, 2, 2, 3);
+		actual = lattice.searchOperationSequence();
+		expected = "CCIIIIIRCC";
+		assertEquals(expected, actual);
+		assertEquals(13, lattice.getDistance());
+	
+		input = "C";
+		output = "DD";
+		lattice = new LevenshteinLattice(input, output, 2, 2, 3);
+		actual = lattice.searchOperationSequence();
+		System.err.println(actual);
+		assertEquals(5, lattice.getDistance());
+		expected = "IR";
+		assertEquals(expected, actual);
+		
+		input = "A CB";
+		output = "A DDDDD B";
+		lattice = new LevenshteinLattice(input, output, 2, 2, 3);
+		actual = lattice.searchOperationSequence();
+		assertEquals(13, lattice.getDistance());
+		expected = "CCIIIIIRC";
+		assertEquals(expected, actual);
+		
+
+	}
+	
+	@Test
 	public void test() {
 		String input, output, expected, actual;
 		LevenshteinLattice lattice;
--- trunk/marmot/src/marmot/tokenize/openlp/Aligner.java
+++ trunk/marmot/src/marmot/tokenize/openlp/Aligner.java
@@ -5,10 +5,31 @@ import java.util.List;
 public interface Aligner {
 
 	class Pair {
-		int a;
-		int b;
+		public int a;
+		public int b;
+		
+		public Pair(int a, int b) {
+			this.a = a;
+			this.b = b;
+		}
+		
+		@Override
+		public String toString() {
+			return String.format("(%d, %d)", a, b);
+		}	
+		
+		@Override
+		public boolean equals(Object other) {
+			if (!(other instanceof Pair)) {
+				return false;
+			}
+			
+			Pair other_pair = (Pair) other;
+			
+			return a == other_pair.a && b == other_pair.b;
+		}
+				
 	}
 	
 	List<Pair> align(String a, String b);
-
 }
