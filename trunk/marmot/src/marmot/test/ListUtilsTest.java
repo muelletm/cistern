e704b0e7757bb4246416c3fa899099542281b195
muelletm@gmail.com <muelletm@gmail.com@a95995bb-a1a2-3262-07d2-502f3465652d>
2014-11-07 07:32:54 +0000

    Added a CrossAnnotator utility.

---
+++ trunk/marmot/src/marmot/test/ListUtilsTest.java
@@ -0,0 +1,94 @@
+package marmot.test;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import marmot.util.ListUtils;
+
+import org.junit.Test;
+
+public class ListUtilsTest {
+
+	@Test
+	public void complementTest() {
+		List<List<Integer>> items; 
+		List<Integer> expected, actual;
+		
+		items = new LinkedList<>();
+		items.add(Arrays.asList(1, 2));
+		items.add(Arrays.asList(3, 4));
+		items.add(Arrays.asList(5, 6));
+		items.add(Arrays.asList(7, 8));
+	
+		expected = Arrays.asList(1, 2, 3, 4, 7, 8);
+		actual = ListUtils.complement(items, 2);
+		assertEquals(expected, actual);
+		
+		expected = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+		actual = ListUtils.complement(items, 4);
+		
+		assertEquals(expected, actual);
+		actual = ListUtils.complement(items, -1);
+		assertEquals(expected, actual);
+		
+	}
+	
+	@Test
+	public void chunkTest() {
+		List<List<Integer>> expected, actual;
+		
+		List<Integer> items = Arrays.asList(1, 2, 3, 4, 5, 6 , 7, 8);
+		
+		expected = new LinkedList<>();
+		expected.add(Arrays.asList(1, 2));
+		expected.add(Arrays.asList(3, 4));
+		expected.add(Arrays.asList(5, 6));
+		expected.add(Arrays.asList(7, 8));
+		
+		actual = ListUtils.chunk(items, 4);
+		assertEquals(expected, actual);
+		
+		expected = new LinkedList<>();
+		expected.add(Arrays.asList(1));
+		expected.add(Arrays.asList(2));
+		expected.add(Arrays.asList(3));
+		expected.add(Arrays.asList(4));
+		expected.add(Arrays.asList(5, 6, 7, 8));
+		actual = ListUtils.chunk(items, 5);
+		assertEquals(expected, actual);
+		
+		expected = new LinkedList<>();
+		expected.add(Arrays.asList(1));
+		expected.add(Arrays.asList(2));
+		expected.add(Arrays.asList(3));
+		expected.add(Arrays.asList(4));
+		expected.add(Arrays.asList(5));
+		expected.add(Arrays.asList(6));
+		expected.add(Arrays.asList(7));
+		expected.add(Arrays.asList(8));
+		actual = ListUtils.chunk(items, 8);
+		assertEquals(expected, actual);
+		
+		expected = new LinkedList<>();
+		expected.add(Arrays.asList(1));
+		expected.add(Arrays.asList(2));
+		expected.add(Arrays.asList(3));
+		expected.add(Arrays.asList(4));
+		expected.add(Arrays.asList(5));
+		expected.add(Arrays.asList(6));
+		expected.add(Arrays.asList(7));
+		expected.add(Arrays.asList(8));
+		actual = ListUtils.chunk(items, 9);
+		assertEquals(expected, actual);
+		
+		expected = Collections.singletonList(items);
+		actual = ListUtils.chunk(items, 1);
+		assertEquals(expected, actual);
+		
+	}
+
+}
---
+++ trunk/marmot/src/marmot/util/ListUtils.java
@@ -0,0 +1,42 @@
+package marmot.util;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListUtils {
+
+	public static <T> List<T> complement(List<List<T>> items, int i) {
+		List<T> complement = new LinkedList<>();
+		for (int j = 0; j < items.size(); j++) {
+			if (i != j) {
+				complement.addAll(items.get(j));
+			}
+		}
+		return complement;
+	}
+
+	public static <T> List<List<T>> chunk(List<T> items, int num_chunks) {
+
+		List<List<T>> chunks = new ArrayList<List<T>>(num_chunks);
+
+		int items_per_chunk = items.size() / num_chunks;
+		if (items_per_chunk == 0)
+			items_per_chunk = 1;
+		
+		List<T> chunk = new ArrayList<T>(items_per_chunk);
+		for (T item : items) {
+			chunk.add(item);
+			
+			if (chunk.size() == items_per_chunk) {
+				chunks.add(chunk);
+				if (chunks.size() < num_chunks)
+					chunk = new LinkedList<T>();
+			}
+		}
+
+		assert chunks.size() == num_chunks;
+		return chunks;
+	}
+
+}
