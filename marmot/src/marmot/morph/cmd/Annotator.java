b58c18d20ef2c06427d55a15e0659d48dec7469b
muelletm@gmail.com <muelletm@gmail.com@a95995bb-a1a2-3262-07d2-502f3465652d>
2014-11-07 07:32:54 +0000

    Added a CrossAnnotator utility.

--- marmot/src/marmot/morph/cmd/Annotator.java
+++ marmot/src/marmot/morph/cmd/Annotator.java
@@ -72,81 +72,86 @@ public class Annotator {
 		SentenceReader reader = new SentenceReader(text_file);
 		
 		for (Sequence sequence : reader) {
-			Sentence sentence = (Sentence) sequence;
-			
-			if (sentence.isEmpty()) {
-				System.err.println("Warning: Skipping empty sentence!");
-				continue;
-			}
-			
-			List<List<String>> tags;
-			
-			try {
-			
-			tags = tagger.tag(sentence);
-			
-			} catch (OutOfMemoryError e) {
-				
-				tags = new ArrayList<List<String>>(sentence.size());
-				
-				List<String> tag = Collections.singletonList("_");
-				
-				for (int index = 0; index < sentence.size(); index ++) {
-					tags.add(tag);
-				}
-				
-				System.err.format("Warning: Can't tag sentence of length: %d (Not enough memory)!\n", sentence.size());
-				
-			}
-			
-			for (int i = 0; i < sentence.size(); i ++) {
-				Word word = sentence.getWord(i);
-				
-				writer.append(Integer.toString(i + 1));
-				writer.append(SEPERATOR_);
-				writer.append(word.getWordForm());
-				
-				// Lemma
-				writer.append(SEPERATOR_);
-				writer.append(EMPTY_);
-				writer.append(SEPERATOR_);
-				writer.append(EMPTY_);
-				
-				// Pos
-				writer.append(SEPERATOR_);
-				writer.append((word.getPosTag() != null ) ? word.getPosTag() : EMPTY_ );
-				writer.append(SEPERATOR_);
-				writer.append(tags.get(i).get(0));
-				
-				// Feat
-				writer.append(SEPERATOR_);
-				writer.append((word.getMorphTag() != null ) ? word.getMorphTag() : EMPTY_);
-				writer.append(SEPERATOR_);
-				writer.append((tags.get(i).size() > 1) ? tags.get(i).get(1) : EMPTY_);
+			annotate(tagger, sequence, writer);
+		}
+	}
 
-//				// Head
-//				writer.append(SEPERATOR_);
-//				writer.append(EMPTY_);
-//				writer.append(SEPERATOR_);
-//				writer.append(EMPTY_);
-//				
-//				// Deprel
-//				writer.append(SEPERATOR_);
-//				writer.append(EMPTY_);
-//				writer.append(SEPERATOR_);
-//				writer.append(EMPTY_);
-//
-//				// Predicate
-//				writer.append(SEPERATOR_);
-//				writer.append(EMPTY_);
-//				
-//				// Yield
-//				writer.append(SEPERATOR_);
-//				writer.append(EMPTY_);
-					
-				writer.append('\n');
+	public static void annotate(Tagger tagger, Sequence sequence, Writer writer) throws IOException {
+		Sentence sentence = (Sentence) sequence;
+		
+		if (sentence.isEmpty()) {
+			System.err.println("Warning: Skipping empty sentence!");
+			return;
+		}
+		
+		List<List<String>> tags;
+		
+		try {
+		
+		tags = tagger.tag(sentence);
+		
+		} catch (OutOfMemoryError e) {
+			
+			tags = new ArrayList<List<String>>(sentence.size());
+			
+			List<String> tag = Collections.singletonList("_");
+			
+			for (int index = 0; index < sentence.size(); index ++) {
+				tags.add(tag);
 			}
+			
+			System.err.format("Warning: Can't tag sentence of length: %d (Not enough memory)!\n", sentence.size());
+			
+		}
+		
+		for (int i = 0; i < sentence.size(); i ++) {
+			Word word = sentence.getWord(i);
+			
+			writer.append(Integer.toString(i + 1));
+			writer.append(SEPERATOR_);
+			writer.append(word.getWordForm());
+			
+			// Lemma
+			writer.append(SEPERATOR_);
+			writer.append(EMPTY_);
+			writer.append(SEPERATOR_);
+			writer.append(EMPTY_);
+			
+			// Pos
+			writer.append(SEPERATOR_);
+			writer.append((word.getPosTag() != null ) ? word.getPosTag() : EMPTY_ );
+			writer.append(SEPERATOR_);
+			writer.append(tags.get(i).get(0));
+			
+			// Feat
+			writer.append(SEPERATOR_);
+			writer.append((word.getMorphTag() != null ) ? word.getMorphTag() : EMPTY_);
+			writer.append(SEPERATOR_);
+			writer.append((tags.get(i).size() > 1) ? tags.get(i).get(1) : EMPTY_);
+
+//			// Head
+//			writer.append(SEPERATOR_);
+//			writer.append(EMPTY_);
+//			writer.append(SEPERATOR_);
+//			writer.append(EMPTY_);
+//			
+//			// Deprel
+//			writer.append(SEPERATOR_);
+//			writer.append(EMPTY_);
+//			writer.append(SEPERATOR_);
+//			writer.append(EMPTY_);
+//
+//			// Predicate
+//			writer.append(SEPERATOR_);
+//			writer.append(EMPTY_);
+//			
+//			// Yield
+//			writer.append(SEPERATOR_);
+//			writer.append(EMPTY_);
+				
 			writer.append('\n');
 		}
+		writer.append('\n');
+	
 	}
 }
---
+++ marmot/src/marmot/morph/cmd/CrossAnnotator.java
@@ -0,0 +1,97 @@
+package marmot.morph.cmd;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.LinkedList;
+import java.util.List;
+
+import marmot.core.Options;
+import marmot.core.Sequence;
+import marmot.core.Tagger;
+import marmot.morph.MorphEvaluator;
+import marmot.morph.MorphModel;
+import marmot.morph.MorphOptions;
+import marmot.morph.MorphResult;
+import marmot.morph.io.SentenceReader;
+import marmot.util.ListUtils;
+
+public class CrossAnnotator {
+
+	public static void main(String[] args) throws IOException {
+
+		String path = "/home/muellets/Downloads/data";
+
+		int num_chunks = 2;
+		String infile = String.format(
+				"form-index=1,tag-index=4,morph-index=6,%s/tiger-2.2.sml.conll09",
+				path);
+		String outfile = String.format("%s/tiger-2.2.sml.marmot.conll09", path);
+
+		MorphOptions options = new MorphOptions();
+		options.setProperty(Options.VERBOSE, "true");
+		options.setProperty(MorphOptions.NUM_ITERATIONS, "1");
+		
+		annotate(options, infile, outfile, num_chunks);
+
+	}
+
+	public static void annotate(MorphOptions options, String infile,
+			String outfile, int num_chunks) throws IOException {
+
+		List<Sequence> sequences = new LinkedList<>();
+		for (Sequence sequence : new SentenceReader(infile)) {
+			sequences.add(sequence);
+		}
+
+		Writer writer = new BufferedWriter(new FileWriter(outfile));
+		annotate(options, sequences, num_chunks, writer);
+		writer.close();
+
+	}
+
+	public static void annotate(MorphOptions options, List<Sequence> sequences,
+			int num_chunks, Writer writer) throws IOException {
+
+		List<List<Sequence>> chunks = ListUtils.chunk(sequences, num_chunks);
+
+		MorphResult result = null;
+		
+		for (int i = 0; i < num_chunks; i++) {
+			
+			if (options.getVerbose()) {
+				System.err.format("Processing chunk %d\n", i);
+			}
+
+			List<Sequence> chunk = chunks.get(i);
+			List<Sequence> complement = ListUtils.complement(chunks, i);
+
+			Tagger tagger = MorphModel.train(options, complement, chunk);
+
+			for (Sequence sequence : chunk) {
+				Annotator.annotate(tagger, sequence, writer);
+			}
+			
+			if (options.getVerbose()) {
+				MorphEvaluator eval = new MorphEvaluator(chunk);
+				
+				MorphResult chunk_result = eval.eval(tagger);
+				if (result == null) {
+					result = chunk_result;
+				} else {
+					result.increment(chunk_result);
+				}
+			}
+			
+			if (result != null) {
+				System.err.println();
+				System.err.println();
+				System.err.println("Overall result:");
+				System.err.println(result);
+			}
+
+		}
+	}
+
+}
