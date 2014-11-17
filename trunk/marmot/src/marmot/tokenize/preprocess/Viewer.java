f9dc63c9eaa8225fa4f925b8945526cb4e0ff046
francesca.caratti@gmail.com <francesca.caratti@gmail.com@a95995bb-a1a2-3262-07d2-502f3465652d>
2014-11-17 08:11:03 +0000

    Added marmot.tokenize.preprocess.Viewer.java, which prints the sentences and pairs to the command line, formatted for viewing ease.

---
+++ trunk/marmot/src/marmot/tokenize/preprocess/Viewer.java
@@ -0,0 +1,99 @@
+package marmot.tokenize.preprocess;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import marmot.tokenize.openlp.Aligner.Pair;
+
+public class Viewer {
+	private List<Pair> pairs_;
+	private String tokenized_;
+	private String untokenized_;
+	
+	public Viewer(String tokenized, String untokenized, List<Pair> pairs) {
+		pairs_ = pairs;
+		tokenized_ = tokenized;
+		untokenized_ = untokenized;		
+	}
+	
+	public List<String> getView() {
+		if(pairs_ == null) return null;
+		
+		String tokenized = "|" + tokenized_.charAt(0);
+		String untokenized = pairs_.get(0).b == 0 ? "|" + untokenized_.charAt(0) : "|\\";
+		
+		String tok_nums = "|0";
+		String untok_nums = "|"+pairs_.get(0).b;
+		
+		int prev = pairs_.get(0).b;
+		
+		for(int i=1; i<pairs_.size(); i++) {
+			int a = pairs_.get(i).a;
+			int b = pairs_.get(i).b;
+			
+			String num_spaces_a = spaceDiff(b, a);
+			String num_spaces_b = spaceDiff(a, b);
+			
+			String diff_a = spaceDiff(a, 1);
+			String diff_b = spaceDiff(b, 1);
+			
+			String num_spaces = diff_a.length() < diff_b.length() ? diff_b : diff_a;
+			
+			if(b == -1) {
+				tok_nums += "|" + i + num_spaces_a;
+				untok_nums += "|" +-1 + num_spaces_b;
+				
+				tokenized += "|" + tokenized_.charAt(a) + num_spaces;
+				untokenized += "|\\" + num_spaces;
+				
+				continue;
+			}
+			if(b == prev) {
+				tok_nums += " " + i + num_spaces_a;
+				untok_nums += " " + prev + num_spaces_b;
+				
+				tokenized += " " + tokenized_.charAt(a) + num_spaces;
+				untokenized += "  " + num_spaces;
+				
+				continue;
+			}
+			
+			tok_nums += "|" + a + num_spaces_a;
+			untok_nums += "|" + b + num_spaces_b;
+			
+			tokenized += "|" + tokenized_.charAt(a) + num_spaces;
+			untokenized += "|" + untokenized_.charAt(b) + num_spaces;
+			
+			prev = b;
+		}
+		
+		List<String> ret = new LinkedList<String>();
+		ret.add(tokenized);
+		ret.add(untokenized);
+		
+		ret.add(tok_nums);
+		ret.add(untok_nums);
+
+		return ret;
+	}
+	
+	/**
+	 * If the spaces for a are fewer than for b, the difference is returned
+	 * @param a
+	 * @param b
+	 * @return difference, given that a is 'smaller' than b
+	 */
+	private String spaceDiff(int a, int b) {
+		int a_count = a < 0 ? 1 : 0;
+		a_count += (int)(Math.log10(Math.abs(a)));
+		
+		int b_count = b < 0 ? 1 : 0;
+		b_count += (int)(Math.log10(Math.abs(b)));
+		
+		String ret = "";
+		for(int i=0; i<(a_count-b_count); i++) {
+			ret += " ";
+		}		
+		return ret;
+	}
+}
