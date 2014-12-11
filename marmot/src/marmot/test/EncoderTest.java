4c0dd554e05b6a92ba31d168bb0a0d17157359a7
muelletm@gmail.com <muelletm@gmail.com@a95995bb-a1a2-3262-07d2-502f3465652d>
2014-12-11 12:18:53 +0000

    Fixed a small bug in Encoder.bitsNeeded. Added a test case.

---
+++ marmot/src/marmot/test/EncoderTest.java
@@ -0,0 +1,27 @@
+package marmot.test;
+
+import static org.junit.Assert.*;
+import marmot.util.Encoder;
+
+import org.junit.Test;
+
+public class EncoderTest {
+
+	@Test
+	public void testBitsNeeded() {
+		assertEquals(1, Encoder.bitsNeeded(0));
+		assertEquals(1, Encoder.bitsNeeded(1));
+		assertEquals(2, Encoder.bitsNeeded(2));
+		assertEquals(2, Encoder.bitsNeeded(3));
+		assertEquals(3, Encoder.bitsNeeded(4));
+		assertEquals(3, Encoder.bitsNeeded(5));
+		assertEquals(3, Encoder.bitsNeeded(6));
+		assertEquals(3, Encoder.bitsNeeded(7));
+		assertEquals(4, Encoder.bitsNeeded(8));
+		
+		for (int i = 0; i < 1000; i++) {
+			int expected_num_bits = Integer.toBinaryString(i).length();
+			assertEquals(expected_num_bits, Encoder.bitsNeeded(i));
+		}
+	}
+}
--- marmot/src/marmot/util/Encoder.java
+++ marmot/src/marmot/util/Encoder.java
@@ -14,16 +14,20 @@ public class Encoder {
 	private short current_array_length_;
 	private short current_bit_index_;
 
-	private static final double log2_ = Math.log(2);
-	
 	public Encoder(int capacity) {
 		bytes_ = new int[capacity];
 		reset();
 	}
 
 	public static int bitsNeeded(int max_value) {
-		int num_bits = (int) Math.ceil((Math.log(max_value + 1) / log2_));
-		// rangeCheck(current_array_length_ + num_bits - ((8 * Integer.SIZE) - current_bit_index_));
+		int num_bits = 1;
+		while (true) {
+			max_value /= 2;		
+			if (max_value == 0) {
+				break;
+			}
+			num_bits += 1;
+		}		
 		return num_bits;
 	}
 
@@ -32,16 +36,9 @@ public class Encoder {
 	}
 
 	public void append(int value, int bits_needed) {
-		//System.err.println(value + " " + bits_needed);
-		
 		assert value >= 0;
 		assert bitsNeeded(value) <= bits_needed;
-		
-		//assert value <= max_value;
-		//int bits_needed = bitsNeeded(max_value);
-		
-		//System.err.println("bits nedded: " + bits_needed);
-		
+
 		while (bits_needed != 0) {
 			if (current_bit_index_ == Integer.SIZE) {
 				current_array_length_ ++;
@@ -49,33 +46,15 @@ public class Encoder {
 			}
 
 			int bits_left = Integer.SIZE - current_bit_index_;
-			//System.err.println("bits left: " + bits_left);
-			
 			int bits = Math.min(bits_left, bits_needed);
-			//System.err.println("bits: " + bits);
-			
-
-			//System.err.println("max value: " + Integer.toBinaryString(max_value));
-			//System.err.println("value: " + Integer.toBinaryString(value));
-
-			int mask = (2 << (bits - 1)) - 1;
-			
-			//System.err.println("mask: " + Integer.toBinaryString(mask));
-			
+			int mask = (2 << (bits - 1)) - 1;			
 			int b = value & mask;
 			value >>= bits;
 			bits_needed -= bits;
 			
-			//System.err.println("b: " + Integer.toBinaryString(b));
-			bytes_[current_array_length_ - 1] += b << current_bit_index_;	
-			
-			//System.err.println("byte: " + Integer.toBinaryString(bytes_[current_array_length_ - 1]));
+			bytes_[current_array_length_ - 1] += b << current_bit_index_;				
 			current_bit_index_ += bits;
 		}
-		
-		
-		//System.err.println(Arrays.toString(bytes_));
-		
 		assert value == 0;
 	}
 
@@ -89,14 +68,6 @@ public class Encoder {
 		current_array_length_ = 0;
 		current_bit_index_ = Integer.SIZE;
 		Arrays.fill(bytes_, 0);
-	}
-
-	public static void main(String[] args) {
-		Encoder encoder = new Encoder(5);
-		encoder.append(3, 5);
-		encoder.append(7, 12);
-		encoder.append(1, 1);
-		encoder.append(241342312, 250000000);
 	}
 
 	public Feature getFeature(boolean flag) {
