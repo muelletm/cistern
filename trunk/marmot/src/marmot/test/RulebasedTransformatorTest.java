9f7ad6e2df5eb14b8eb58583de92f72cd01dcfb7
muelletm@gmail.com <muelletm@gmail.com@a95995bb-a1a2-3262-07d2-502f3465652d>
2014-12-17 16:09:55 +0000

    Some simplifications in RuleProvider. SplitRules is now called RulebasedTransformator and only works on a single string. Fixed some test cases. Simplified RulebasedTransformator.applyRules.

---
+++ trunk/marmot/src/marmot/test/RulebasedTransformatorTest.java
@@ -0,0 +1,52 @@
+package marmot.test;
+
+import static org.junit.Assert.*;
+import marmot.tokenize.rules.RuleProvider;
+import marmot.tokenize.rules.RulebasedTransformator;
+
+import org.junit.Test;
+
+public class RulebasedTransformatorTest {
+
+	public void testTokRules(String lang, String input, String expected){
+		RuleProvider p = RuleProvider.createRuleProvider(lang);
+		RulebasedTransformator r = p.getTokTransformator();		
+		test(r, input, expected);
+	}
+	
+	public void testUnTokRules(String lang, String input, String expected){
+		RuleProvider p = RuleProvider.createRuleProvider(lang);
+		RulebasedTransformator r = p.getUnTokTransformator();		
+		test(r, input, expected);
+	} 
+
+	private void test(RulebasedTransformator r, String input, String expected) {
+		String actual = r.applyRules(input);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testSpanishTokRules() {
+		testUnTokRules("es", "no match.", "no match.");
+		testUnTokRules("es"," del. ", " de el. ");
+		
+		testUnTokRules("es"," adela del ", " adela de el ");
+		testUnTokRules("es"," ádela del ", " ádela de el ");
+		testUnTokRules("es"," ödela del ", " ödela de el ");
+		testUnTokRules("es"," ñdela del ", " ñdela de el ");
+		
+		testUnTokRules("es", " del ", " de el ");
+		testUnTokRules("es", "del", "de el");
+		testUnTokRules("es", " del", " de el");
+		testUnTokRules("es", "del ", "de el ");
+	}
+	
+	@Test 
+	public void testSpanishUnTokRules() {
+		testTokRules("es", "dejando_de_lado", "dejando de lado");		
+		testTokRules("es", " 1.0 Fz ", " 1.0 ");
+		testTokRules("es", " 1.0 FZ ", " 1.0 FZ ");
+		testTokRules("es", " 1.0 Fz", " 1.0");	
+	}
+
+}
---
+++ trunk/marmot/src/marmot/tokenize/rules/RulebasedTransformator.java
@@ -0,0 +1,46 @@
+package marmot.tokenize.rules;
+
+import java.util.Collection;
+import java.util.regex.Matcher;
+
+public class RulebasedTransformator {
+	/**
+	 * This class can define a number of conversion rules for exceptions which arise in either
+	 * direction of tokenization. After finding a match in the corresponding string, a 
+	 * confirmation match is searched for in the other one. Apart from confirming, little can
+	 * be done though, since this code is run strictly before alignment, so we don't know
+	 * anything about corresponding positions between the two strings.
+	 * After confirmation, the initial match is replaced with the confirmation match.
+	 */
+	private Collection<Rule> rules_;
+		
+	public RulebasedTransformator(Collection<Rule> rules) {
+		rules_ = rules;
+	}
+
+	public String applyRules(String string) {
+		return applyRules(string, rules_);
+	}
+	
+	private String applyRules(String string, Collection<Rule> rules) {
+		StringBuilder sb = new StringBuilder();
+		
+		for(Rule rule : rules) {
+			Matcher matcher = rule.pattern.matcher(string);
+
+			int end = 0;
+			while(matcher.find()) {			
+				int start = matcher.start(1);
+				sb.append(string.substring(end, start));
+				sb.append(rule.replacement);
+				end   = matcher.end(1);
+			}
+			sb.append(string.substring(end));
+			
+			string = sb.toString();
+			// Reset string builder.
+			sb.setLength(0);
+		}	
+		return string;
+	}
+}
