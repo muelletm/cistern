f313c1b0cdc1b32aa4f77a3044fdda02e7c65c8a
francesca.caratti@gmail.com <francesca.caratti@gmail.com@a95995bb-a1a2-3262-07d2-502f3465652d>
2014-12-17 12:30:49 +0000

    Added comments to SplitRules.java and WikiSelector.java.

--- trunk/marmot/src/marmot/tokenize/openlp/SplitRules.java
+++ trunk/marmot/src/marmot/tokenize/openlp/SplitRules.java
@@ -6,10 +6,21 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SplitRules {
+	/**
+	 * This class can define a number of conversion rules for exceptions which arise in either
+	 * direction of tokenization. After finding a match in the corresponding string, a 
+	 * confirmation match is searched for in the other one. Apart from confirming, little can
+	 * be done though, since this code is run strictly before alignment, so we don't know
+	 * anything about corresponding positions between the two strings.
+	 * After confirmation, the initial match is replaced with the confirmation match.
+	 */
 	private Map<Matcher, Matcher> tok_rules_;
 	private Map<Matcher, Matcher> untok_rules_;
 	
+	
 	public SplitRules() {
+		// initialize matchlist. The setup is very specific, so it's not handled 
+		// in a user-accesses method.
 		tok_rules_ = new HashMap<Matcher, Matcher>();
 		untok_rules_ = new HashMap<Matcher, Matcher>();
 		addRule("del", "de el", untok_rules_);
@@ -18,6 +29,7 @@ public class SplitRules {
 	}
 	
 	public String[] applyRules(String untok, String tok) {
+		// calls the rule sets for both directions and sends the results back
 		String[] result = applyRule(tok, untok, tok_rules_);
 		tok = result[0];
 		untok = result[1];
@@ -25,6 +37,7 @@ public class SplitRules {
 	}
 	
 	private void addRule(String a, String b, Map<Matcher, Matcher> rule_book) {
+		// internal encapsulation of the rule-adding routine. Note that it's private 
 		rule_book.put(
 				Pattern.compile("\\W"+a+"\\W", Pattern.CASE_INSENSITIVE).matcher(""), 
 				Pattern.compile("\\W"+b+"\\W", Pattern.CASE_INSENSITIVE).matcher("")
@@ -34,6 +47,7 @@ public class SplitRules {
 	
 	
 	private String[] applyRule(String a, String b, Map<Matcher, Matcher> rule_book) {
+		// application of either of the two rule sets
 		for(Matcher key : rule_book.keySet()) {
 			Matcher value = rule_book.get(key);
 			String replace_string = value.pattern().pattern();
