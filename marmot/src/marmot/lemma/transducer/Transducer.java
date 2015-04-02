0a3e1ef0252de5c2dacc07c059b1532c1aeca471
Ryan Cotterell <ryan.cotterell@gmail.com>
2015-04-02 13:26:54 +0200

    added guava jar

--- marmot/src/marmot/lemma/transducer/Transducer.java
+++ marmot/src/marmot/lemma/transducer/Transducer.java
@@ -1,11 +1,34 @@
+import java.util.Set;
 
 public abstract class Transducer {
 	
-	public Transduer() {
+	// context values
+	// See Cotterell et al. (2014) for more details
+	// c1 = upper left hand context
+	// c2 = upper right hand context
+	// c3 = lower left hand context
+	// c4 = lower right hand context
+	private int c1;
+	private int c2;
+	private int c3;
+	private int c4;
+	
+	private Set alphabet;
+	
+	public Transduer(Set alphabet, int c1, int c2, int c3, int c4) {
+		this.c1 = c1;
+		this.c2 = c2;
+		this.c3 = c3;
+		this.c4 = c4;
+		
+		this.alphabet = alphabet;
+	}
+	
+	private void extractContexts() {
 		
 	}
 	
-	private void gradient()
-	private double log_likelihood()
+	private abstract void gradient();
+	private abstract double logLikelihood();
 	
 }
