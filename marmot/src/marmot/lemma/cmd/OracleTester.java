e08da835de232773f19cd5653163f56503746bbf
Thomas Mueller <muelletm@gmail.com>
2015-04-22 17:56:29 +0200

    Missing new file.

---
+++ marmot/src/marmot/lemma/cmd/OracleTester.java
@@ -0,0 +1,19 @@
+// Copyright 2015 Thomas Müller
+// This file is part of MarMoT, which is licensed under GPLv3.
+
+package marmot.lemma.cmd;
+
+import marmot.lemma.LemmatizerGenerator;
+import marmot.lemma.Result;
+import marmot.util.FileUtils;
+
+public class OracleTester {
+
+	public static void main(String[] args) {		
+		String model_file = args[0];
+		String test_file = args[1];
+		LemmatizerGenerator lemmatizer = FileUtils.loadFromFile(model_file);
+		Result.testGenerator(lemmatizer, test_file).logAccuracy();
+	}
+
+}
