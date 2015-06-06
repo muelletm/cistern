538ae23f0f297cf952bf369206455130e6659868
Ryan Cotterell <ryan.cotterell@gmail.com>
2015-06-07 01:57:00 +0200

    fixed titel

--- marmot/scripts/ising_ryan.sh
+++ marmot/scripts/ising_ryan.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-java -cp /Users/ryancotterell/Documents/research/cistern/marmot/bin:/Users/ryancotterell/Documents/research/cistern/marmot/lib/javatuples-1.2.jar marmot.ising.BruteForceUnit
+#java -cp /Users/ryancotterell/Documents/research/cistern/marmot/bin:/Users/ryancotterell/Documents/research/cistern/marmot/lib/javatuples-1.2.jar marmot.ising.BruteForceUnit
+
+#java -cp /Users/ryancotterell/Documents/research/cistern/marmot/bin:/Users/ryancotterell/Documents/research/cistern/marmot/lib/javatuples-1.2.jar marmot.ising.Analyzer italian
+
+java -cp /Users/ryancotterell/Documents/research/cistern/marmot/bin:/Users/ryancotterell/Documents/research/cistern/marmot/lib/javatuples-1.2.jar marmot.ising.Analyzer ../data/current_version/morph-it_048.txt
--- marmot/src/marmot/ising/DataReader.java
+++ marmot/src/marmot/ising/DataReader.java
@@ -1,5 +1,27 @@
 package marmot.ising;
 
-public class DataReader {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+import org.javatuples.Pair;
+
+public class DataReader {
+	
+	
+	protected Map<String,Integer> atoms2Integer;
+	protected List<Datum> data;
+	protected Set<Pair<Integer, Integer>> pairs;
+	
+	
+	public DataReader() {
+		
+		this.atoms2Integer = new HashMap<String,Integer>();
+		this.data = new ArrayList<Datum>();
+		this.pairs = new HashSet<Pair<Integer, Integer>>();
+		
+	}
 }
