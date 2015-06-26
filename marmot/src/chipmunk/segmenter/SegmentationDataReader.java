a1a94cbc55a1b658177a17e7d8e9e82a787bb676
Thomas Mueller <muelletm@gmail.com>
2015-06-26 20:33:22 +0200

    Fixed bug related to new iterative capabilities of SegmentationDataReader.

--- marmot/src/chipmunk/segmenter/SegmentationDataReader.java
+++ marmot/src/chipmunk/segmenter/SegmentationDataReader.java
@@ -10,20 +10,23 @@ import marmot.util.LineIterator;
 
 public class SegmentationDataReader implements Iterable<Word> {
 
+	private List<Word> words_;
 	private int tag_level_;
 	private StringNormalizer normalizer_;
 	private Map<String, Word> vocab_;
 	private String filepath_;
-	
+
 	public SegmentationDataReader(String filepath, String lang, int tag_level) {
-		
 		normalizer_ = StringNormalizer.labeledCreate(lang);
 		tag_level_ = tag_level;
-		
 		filepath_ = filepath;
+		words_ = null;
+		vocab_ = null;
 	}
-	
+
 	public List<Word> map(List<Word> words) {
+		getData();
+		
 		List<Word> new_words = new LinkedList<>();
 		for (Word word : words) {
 			Word new_word = vocab_.get(word.getWord());
@@ -32,22 +35,23 @@ public class SegmentationDataReader implements Iterable<Word> {
 		}
 		return new_words;
 	}
-	
+
 	public List<Word> getData() {
-		vocab_ = new HashMap<>();
-		List<Word> words = new LinkedList<Word>();
-		for (Word word : this) {
-			words.add(word);
+		if (words_ == null) {
+			vocab_ = new HashMap<>();
+			words_ = new LinkedList<Word>();
+			for (Word word : this) {
+				words_.add(word);
+			}
 		}
-		vocab_ = null;
-		return words;
+		return words_;
 	}
 
 	@Override
 	public Iterator<Word> iterator() {
-		
+
 		final LineIterator iterator = new LineIterator(filepath_, "\t");
-		
+
 		return new Iterator<Word>() {
 
 			@Override
@@ -57,33 +61,32 @@ public class SegmentationDataReader implements Iterable<Word> {
 
 			@Override
 			public Word next() {
-				
+
 				List<String> line = iterator.next();
 				assert line.size() == 2;
-				
-				
-					String word_string = line.get(0);
-					
-					word_string = normalizer_.normalize(word_string);
-					
-					Word word = null;
+
+				String word_string = line.get(0);
+
+				word_string = normalizer_.normalize(word_string);
+
+				Word word = null;
+				if (vocab_ != null)
+					word = vocab_.get(word_string);
+
+				if (word == null) {
+					word = new Word(word_string);
+
 					if (vocab_ != null)
-						word = vocab_.get(word_string);
-					
-					if (word == null) {
-						word = new Word(word_string);
-						
-						if (vocab_ != null)
-							vocab_.put(word_string, word);
-					}
-					
-					if (line.size() > 1) {
-						addReading(word, line.get(1));
-					}
-					
+						vocab_.put(word_string, word);
+				}
+
+				if (line.size() > 1) {
+					addReading(word, line.get(1));
+				}
+
 				return word;
 			}
-				
+
 			@Override
 			public void remove() {
 				throw new UnsupportedOperationException();
@@ -92,65 +95,64 @@ public class SegmentationDataReader implements Iterable<Word> {
 	}
 
 	private void addReading(Word word, String reading_string) {
-		
-		String[] word_readings = reading_string.split(", "); 
-		
+
+		String[] word_readings = reading_string.split(", ");
+
 		for (String word_reading : word_readings) {
-			
+
 			String[] readings = word_reading.split(" ");
 			List<String> segments = new LinkedList<>();
 			List<String> tags = new LinkedList<>();
-			
+
 			for (String reading : readings) {
 
 				int index = -1;
-				for (int i=0; i< reading.length(); i++) {
-					
+				for (int i = 0; i < reading.length(); i++) {
+
 					char c = reading.charAt(i);
-					
+
 					if (c == ':') {
-						
+
 						index = i;
-						if (i  + 1< reading.length() && reading.charAt(i + 1) == ':') {
+						if (i + 1 < reading.length()
+								&& reading.charAt(i + 1) == ':') {
 							index = i + 1;
 						}
-						
+
 						break;
-						
+
 					}
-					
+
 				}
-				
+
 				String segment;
 				String tag;
-				
+
 				if (index < 0 || index == reading.length() - 1) {
 					segment = reading;
 					tag = null;
 				} else {
 					segment = reading.substring(0, index);
-					tag = reading.substring(index + 1);	
+					tag = reading.substring(index + 1);
 				}
-				
+
 				segment = normalizer_.normalize(segment);
 				tag = TagSet.getTag(tag, tag_level_);
-				
+
 				segments.add(segment);
 				tags.add(tag);
 			}
-			
+
 			StringBuilder rejoint_word = new StringBuilder(word.getLength());
 			for (String segment : segments) {
 				rejoint_word.append(segment);
 			}
-			assert rejoint_word.toString().equals(word.getWord()) : reading_string + " " + segments;
-			
-			if (word.getWord().equals("mtk:hon")) {
-				System.err.format("%s %s %s\n", word, segments, tags);
-			}
-			
+
+			assert rejoint_word.toString().equals(word.getWord()) : reading_string
+					+ " " + segments;
+
 			word.add(new SegmentationReading(segments, tags));
 		}
 	}
-	
+
 }
