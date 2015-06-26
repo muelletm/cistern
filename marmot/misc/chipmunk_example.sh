f2f08d9897b9f80a95740ec7162b42acfbd04c3f
Thomas Mueller <muelletm@gmail.com>
2015-06-26 14:25:05 +0200

    Some work on example script.

--- marmot/misc/chipmunk_example.sh
+++ marmot/misc/chipmunk_example.sh
@@ -2,23 +2,28 @@
 
 set -ue
 
-LIBS=marmot-2015-06-26.jar:mallet.jar:JSAP-2.1.jar
-lang=eng
-
+# Download ChipMunk distribution.
 wget cistern.cis.lmu.de/chipmunk/2015-06-26/{marmot-2015-06-26.jar,mallet.jar,JSAP-2.1.jar}
+
+# Download datasets.
 wget cistern.cis.lmu.de/chipmunk/supplement.tar.gz
 tar xzf supplement.tar.gz
 
+LIBS=marmot-2015-06-26.jar:mallet.jar:JSAP-2.1.jar
+lang=eng
+
+# Train model on English data
 java -cp $LIBS chipmunk.segmenter.cmd.Train\
    --train-file supplement/seg/${lang}/trn\
    --lang ${lang}\
+   --verbose false\
    --crf-mode false\
    --tag-level 0\
    --dictionary-paths "supplement/additional/${lang}/aspell.txt supplement/additional/${lang}/wordlist.txt supplement/additional/${lang}/wiktionary.txt"\
    --model-file ${lang}.chipmunk.srl
 
+# Run segmenter on some English words
 echo 'books booked booking bookings rebooked bookstore' | tr ' ' '\n' > input.txt
-
 java -cp $LIBS chipmunk.segmenter.cmd.Segment\
       --model-file ${lang}.chipmunk.srl\
       --input-file input.txt\
